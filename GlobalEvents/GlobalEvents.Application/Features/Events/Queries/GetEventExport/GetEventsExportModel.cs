9189e0b1dc2e2cdc61206f56d058581dbd0d4bda
Ba Nyar Thein <banyarthein@gmail.com>
2025-01-21 12:40:33 +0800

    Started Event Export initial

---
+++ GlobalEvents/GlobalEvents.Application/Features/Events/Queries/GetEventExport/GetEventsExportModel.cs
@@ -0,0 +1,22 @@
+﻿using GlobalEvents.Application.Features.Events.Queries.GetEventDetails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalEvents.Application.Features.Events.Queries.GetEventExport
+{
+    public class GetEventsExportModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Artist { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } =string.Empty;
+    }
+}
---
+++ GlobalEvents/GlobalEvents.Application/Features/Events/Queries/GetEventExport/GetEventsExportQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalEvents.Application.Features.Events.Queries.GetEventExport
+{
+    public class GetEventsExportQueryHandler : IRequestHandler<GetEventsExportQuery, GetEventsExportFile>
+    {
+        public Task<GetEventsExportFile> Handle(GetEventsExportQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
